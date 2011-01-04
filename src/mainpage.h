6b4bd3173693ac76f2a7703bcd8d2655a7c8906c
gonnet <gonnet@016c7d60-3763-4930-9371-3905fd736d95>
2011-01-04 13:27:40 +0000

    add mainpage.h for documentation.

---
+++ src/mainpage.h
@@ -0,0 +1,41 @@
+/*******************************************************************************
+ * This file is part of mdcore.
+ * Coypright (c) 2010 Pedro Gonnet (gonnet@maths.ox.ac.uk)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ ******************************************************************************/
+
+/**
+ * @mainpage @c mdcore
+ *
+ * @section sec_whatis What is mdcore?
+ *
+ * mdcore is an attempt to construct a general-purpose library containing
+ * efficient implementations of the core routines required for Molecular Dynamics
+ * simulations on shared-memory parallel architectures.
+ *
+ * These target architectures include
+ * 
+ * @li Multi-Core Processors,
+ * @li The IBM Cell Broadband Engine,
+ * @li Graphics Processing Units (eventually).
+ *
+ * The goal of this project is not to provide yet another fully-featured simulation
+ * software, but to provide shared-memory functionallity that can be easily
+ * integrated into existing or future simulation codes.
+ *
+ * @section sec_using Using mdcore
+ *
+ */
